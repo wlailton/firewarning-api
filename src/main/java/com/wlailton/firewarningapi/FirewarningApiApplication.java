c6b8e78e428530795e7f0c48d0561b7ff82e442d
wlailton <wlailton@gmail.com>
2019-02-08 15:06:41 -0300

    Project creation.

---
+++ src/main/java/com/wlailton/firewarningapi/FirewarningApiApplication.java
@@ -0,0 +1,14 @@
+package com.wlailton.firewarningapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirewarningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirewarningApiApplication.class, args);
+	}
+
+}
+
