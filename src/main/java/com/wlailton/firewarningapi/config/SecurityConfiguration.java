9ce829abcabd0b9694bc752082bc42026810ba31
wlailton <wlailton@gmail.com>
2019-02-11 21:33:59 -0300

    Implementing Authentication with JWT.

--- src/main/java/com/wlailton/firewarningapi/config/SecurityConfiguration.java
+++ src/main/java/com/wlailton/firewarningapi/config/SecurityConfiguration.java
@@ -1,19 +1,44 @@
 package com.wlailton.firewarningapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.wlailton.firewarningapi.security.JWTAuthenticationFilter;
+import com.wlailton.firewarningapi.security.JWTLoginFilter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-                .authorizeRequests().antMatchers("/console/**").permitAll();
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		
+		httpSecurity.csrf().disable().authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/api/company/companies").permitAll()
+		.antMatchers(HttpMethod.POST, "/login").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		
+		// filter login requests
+		.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                UsernamePasswordAuthenticationFilter.class)
+		
+		// filters other requests to check the presence of JWT in the header
+		.addFilterBefore(new JWTAuthenticationFilter(),
+                UsernamePasswordAuthenticationFilter.class);
+	}
 
-        httpSecurity.csrf().disable();
-        httpSecurity.headers().frameOptions().disable();
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+		// create default accounts
+		.withUser("admin").password("password").roles("ADMIN")
+		.and()
+		.withUser("system").password("password").roles("SYSTEM");
+	}
 
 }
